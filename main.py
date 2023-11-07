50426fa301e0f11de83dd7c2647770ccc8eea249
Vygantas Vilius <biesaz@gmail.com>
2023-11-07 21:15:49 +0200

    learn virtual env

---
+++ main.py
@@ -0,0 +1,10 @@
+
+# python -m venv .venv                  susikuriam
+# source .venv/Scripts/activate       aktyvuojam
+# pip list                              pasitikrinam
+# pip install Flask                   instalinam Flaska
+# flask --app app run                 paleidziam failiuka app
+# pip freeze > requirements.txt         sumetam i txt visas Flasko bibliotekas
+# deactivate                            
+
+# rm -rf venv
