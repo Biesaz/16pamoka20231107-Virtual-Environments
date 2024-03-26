260b80072baa8dd67516f8bbaf47b188ba54e1fc
Vygantas Vilius <biesaz@gmail.com>
2024-03-26 20:44:37 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 
-# python -m venv .venv                  susikuriam
+# python -m venv .venv                susikuriam
 # source .venv/Scripts/activate       aktyvuojam
-# pip list                              pasitikrinam
+# pip list                            pasitikrinam
 # pip install Flask                   instalinam Flaska
 # flask --app app run                 paleidziam failiuka app
-# pip freeze > requirements.txt         sumetam i txt visas Flasko bibliotekas
+# pip freeze > requirements.txt       sumetam i txt visas Flasko bibliotekas
+# pip install -r requirements.txt       instaliuoja kas jau yra sumesta i requirements.txt
 # deactivate                            
 
 # rm -rf venv
+
+
+# Alberto paruostukas:
+
+# python -m venv .venv            --- susikuriam virtualia aplinka
+# source .venv/Scripts/activate   --- pasileidziam virtualia aplinka
+# pip install Flask               --- instaliuojam flask
+# flask --app app run             --- pasirase flask programa app.py ja pasileidziam
+# pip list                        --- pasiziurim ka turim suinstaline
+# pip freeze                      --- tas pats tik su versijom
+# pip freeze --local > requirements.txt   --- surasom ta pati i faila
+# deactivate  
+
+
+
